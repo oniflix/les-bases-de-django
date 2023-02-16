9f24751eef208b86a20fb1b6643233c0857b58e0
Addess <nicolas.cassier@etu.unilim.fr>
2023-02-16 12:53:37 +0100

    bd_updated

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth.models import User
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=35)
+    slug = models.SlugField(default=slugify(name))
+
 
 # Create your models here.
+User
 class BlogPost(models.Model):
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ManyToManyField(Category)
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(default=slugify(title))
     published = models.BooleanField(default=False)
-    date = models.DateField(blank=True)
+    date = models.DateField(blank=True, null=True)
     description = models.TextField(default='')
     content=models.TextField(default='')
+    
+        
+        
+    def published_string(self):
+        if self.published:
+            return "L'article est publié"
+        
+        return "L'article est inaccessible"
+        
